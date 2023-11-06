b4bc427d56d14603c9dac9e9fb0d3cd64a9ab38f
Aaron Rosen <aaronorosen@gmail.com>
2023-11-06 16:45:56 -0500

    update the code more

--- search/management/commands/send_messages.py
+++ search/management/commands/send_messages.py
@@ -3,6 +3,18 @@ from django.core.management.base import BaseCommand
 from twilio.rest import Client
 from django.conf import settings
 
+
+def send_message(client, to, body):
+    twilio_phone_number = settings.TWILIO['TWILIO_PHONE_NUMBER']
+    message = client.messages.create(
+        body=body,
+        from_=twilio_phone_number,
+        to=to
+    )
+    print(message)
+    print(f"Message sent successfully. Message SID: {message.sid}")
+
+
 class Command(BaseCommand):
     help = 'Send Twilio messages'
 
@@ -17,19 +29,6 @@ class Command(BaseCommand):
 
         client = Client(twilio_account_sid, twilio_auth_token)
 
-        def send_message(to, body):
-            try:
-                message = client.messages.create(
-                    body=body,
-                    from_=twilio_phone_number,
-                    to=to
-                )
-                print(f"Message sent successfully. Message SID: {message.sid}")
-            except Exception as e:
-                print(f"Error sending message: {e}")
-
-        to_number = '+18434259777'  
-
+        to_number = '18434259777'  
         message_body = 'Hello from Twilio!'
-
-        send_message(to_number, message_body)
+        send_message(client, to_number, message_body)
